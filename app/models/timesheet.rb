f54164bb072249a13fcd4460d692a275a86d2802
hieu hoang <hieu.h@cs.ventures>
2021-08-15 12:18:23 +0700

    validate

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -16,4 +16,19 @@ class Timesheet < ApplicationRecord
   belongs_to :user
 
   validates :date, :start_time, :finish_time, presence: true
+  validate :time_working, :date_working
+
+  private
+
+  def date_working
+    return unless date
+
+    errors.add(:date, ' working can not be in the future.') if date > Date.today
+  end
+
+  def time_working
+    return unless start_time || finish_time
+    
+    errors.add(:finish_time, 'always greater than or equal Start time.') if start_time > finish_time
+  end
 end
